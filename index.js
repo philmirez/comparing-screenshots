8049300cac75f406e3c626c4e4dcbdc99cadcce1
Phil Ramirez <philmirez@gmail.com>
2019-10-11 00:01:30 -0400

    Full page in memory diff comparison

--- index.js
+++ index.js
@@ -7,67 +7,82 @@ async function takeScreenshot(options) {
     defaultViewport: null
   });
 
-  const page = await browser.newPage();
-
-  page.setViewport({
-    height: options.height,
-    width: options.width
+  const page = await browser.newPage()
+  await page.setViewport({
+    height: 960,
+    width: 640
   })
-
   await page.goto(
     options.url,
-    { waitUntil: 'domcontentloaded' }
-  );
+    { waitUntil: 'networkidle2' }
+  )
+  const bodyHandle = await page.$('body');
+  const { width, height } = await bodyHandle.boundingBox();
+  const screenshot = await page.screenshot({
+    path: options.path || null,
+    clip: {
+      x: 0,
+      y: 0,
+      width: Math.round(width),
+      height: Math.round(height)
+    },
+    type: 'png'
+  });
 
-  const screenshot = await page.screenshot();
-
+  await bodyHandle.dispose();
   await browser.close();
 
-  return screenshot;
+  return {
+    data: screenshot,
+    width: Math.round(width),
+    height: Math.round(height)
+  };
 }
 
 const run = async function () {
-  const options = {
-    width: 640,
-    height: 960,
-    url1: 'https://www.google.com',
-    url2: 'https://www.bing.com'
+  try {
+    const options1 = {
+      path: 'screenshot1.png',
+      url: 'https://www.healthcentral.com'
+    }
+    const options2 = {
+      path: 'screenshot2.png',
+      url: 'https://www.healthcentral.com'
+    }
+
+
+    const [screenshot1, screenshot2] = await Promise.all([takeScreenshot(options1), takeScreenshot(options2)])
+    console.log('screenshot1', screenshot1)
+    console.log('screenshot1 length', screenshot1.data.length)
+    console.log('screenshot2', screenshot2)
+    console.log('screenshot2 length', screenshot2.data.length)
+
+    const maxHeight = Math.max(screenshot1.height, screenshot2.height)
+    const maxWidth = Math.max(screenshot1.width, screenshot2.width)
+    console.log('maxHeight', maxHeight)
+    console.log('maxWidth', maxWidth)
+    const totalPixels = maxHeight * maxWidth
+    const diff = new PNG({ width: maxWidth, height: maxHeight })
+
+    const allocationSize = Math.max(screenshot1.data.length, screenshot2.data.length, diff.data.length)
+    const buff1 = Buffer.alloc(allocationSize)
+    const buff2 = Buffer.alloc(allocationSize)
+    const buffDiff = Buffer.alloc(allocationSize)
+    buff1.fill(screenshot1.data)
+    buff2.fill(screenshot2.data)
+    console.log('buff1 length', buff1.length)
+    console.log('buff2 length', buff2.length)
+
+
+    console.log('diff.data length', diff.data.length)
+    const numDiffPixels = pixelmatch(buff1, buff2, buffDiff, maxWidth, maxHeight, { threshold: 0.2 })
+    console.log('numDiffPixels', numDiffPixels)
+    console.log('Results', JSON.stringify({
+      diffScore: numDiffPixels / totalPixels
+    }))
+  } catch (error) {
+    console.error(error)
   }
-  const totalPixels = options.width * options.height
-
-  const screenshot1 = await takeScreenshot({
-    width: options.width,
-    height: options.height,
-    url: options.url1
-  })
-  console.log('screenshot1', screenshot1)
-  console.log('screenshot1 length', screenshot1.length)
-
-  const screenshot2 = await takeScreenshot({
-    width: options.width,
-    height: options.height,
-    url: options.url2
-  })
-  console.log('screenshot2', screenshot2)
-  console.log('screenshot2 length', screenshot2.length)
-
-  const allocationSize = Math.max(screenshot1.length, screenshot2.length)
-  const buff1 = Buffer.alloc(allocationSize)
-  const buff2 = Buffer.alloc(allocationSize)
-  buff1.fill(screenshot1)
-  buff2.fill(screenshot2)
-  console.log('buff1 length', buff1.length)
-  console.log('buff2 length', buff2.length)
-
-  const diff = new PNG({
-    width: options.width,
-    height: options.height
-  })
-  const numDiffPixels = pixelmatch(buff1, buff2, diff.data, options.width, options.height, { threshold: 0.2 })
-  console.log('Results', JSON.stringify({
-    done: true,
-    diffScore: numDiffPixels / totalPixels
-  }))
 }
 
 run()
